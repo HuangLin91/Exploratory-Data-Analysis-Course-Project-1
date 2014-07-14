7556406812bc2c8274435880f6a0a295b7d153b4
HuangLin91 <chxzhumichael@gmail.com>
2014-07-14 22:23:54 +0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,28 +1,15 @@
 ## plot3
 ## Energy submetering
 
-# will use fread from "data.table"
-library(data.table) 
-library(ggplot2)
-
-## open file connection and donwload .zip file
-tmp<- tempfile()
-download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",tmp)
-
-## get the .txt filename
-dataName <- unzip(tmp, list=TRUE)$Name
-
-# remove link to temp
-unlink(tmp)
-
 # open .txt file for reading
-tmp1 <- file(dataName, "r")
+tmp1 <- file("household_power_consumption.txt", "r")
 
 # find the lines with dates 1/2/2007 through 2/2/2007, save as "proj1.txt"
 cat(grep("(^Date)|(^[1|2]/2/2007)",readLines(tmp1), value=TRUE), sep="\n", file="proj1.txt")
 close(tmp1)
 # read the selected data into memory
-proj1 <- fread("proj1.txt", sep=";", header=TRUE, na.strings="?")
+proj1 <- read.table("proj1.txt", sep=";", header=TRUE, na.strings="?")
+head(proj1)
 
 # convert dates. NB capital "Y" because year=yyyy
 proj1$Date <- as.Date(proj1$Date, "%d/%m/%Y")
